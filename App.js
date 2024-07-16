bbdf661f734d4ddacf50ab3f455dbda0b00b63ec
Alexis Meier <155097923+MeierAlexis@users.noreply.github.com>
2024-07-16 20:53:14 -0300

    Add new Initial Screen

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import NextButton from './components/NextButton'; // Asegúrate de que el componente esté exportado correctamente
+import NextButton from './src/components/NextButton'; // Asegúrate de que el componente esté exportado correctamente
+import InitialScreen from './src/screens/InitialScreen';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Nico</Text>
-      <NextButton text = "Next" />
-      <StatusBar style="auto" />
-    </View>
+      <InitialScreen />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
